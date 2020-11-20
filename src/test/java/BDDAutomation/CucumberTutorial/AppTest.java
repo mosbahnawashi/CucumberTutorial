fef9331d114f1ff349434a014f21629feaa92f3e
Mosbah Alnawashi <mosbah.alnawashi@gmail.com>
2020-11-19 19:19:04 -0800

    Update AppTest.java

--- src/test/java/BDDAutomation/CucumberTutorial/AppTest.java
+++ src/test/java/BDDAutomation/CucumberTutorial/AppTest.java
@@ -3,6 +3,8 @@ package BDDAutomation.CucumberTutorial;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+
+// New chnage.... With Michel...
 /**
  * Unit test for simple App.
  */
